56fa46aa51ec12632853f4cd7137ec7d42a84a3c
Miro1380 <miro1380@gmail.com>
2017-11-04 16:49:55 -0700

    Java REST call. Returns html along with info. Need to fix.

---
+++ src/PracticeREST.java
@@ -0,0 +1,62 @@
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+public class PracticeREST {
+    //Throws a MalformedURLException
+//    URL url = new URL("https://swapi.co/api/");
+
+    public static void main(String []args){
+        new PracticeREST().testIt();
+    }
+
+        private void testIt(){
+
+        String https_url = "https://swapi.co/api/people/3";
+        URL url;
+
+        try{
+            url = new URL(https_url);
+            HttpsURLConnection con = (HttpsURLConnection)url.openConnection();
+
+            //Prints all Info. Even html. Need to fix.
+            print_content(con);
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        catch(IOException u){
+            u.getStackTrace();
+        }
+    }
+
+    //Read data from url ?? Problem >> Prints HTML too ...?
+    private void print_content(HttpsURLConnection con){
+        if(con!=null){
+
+            try {
+
+                System.out.println("****** Content of the URL ********");
+                BufferedReader br =
+                        new BufferedReader(
+                                new InputStreamReader(con.getInputStream()));
+
+                String input;
+
+                while ((input = br.readLine()) != null){
+                    System.out.println(input);
+                }
+                br.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
